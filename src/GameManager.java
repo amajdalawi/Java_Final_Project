7b759c3650fb1dc3f52912ccb13cecd0828f2357
Abudlrahman Almajdalawi <at.majdalawi@gmail.com>
2024-01-02 22:38:40 +0100

    Create the primary classes

---
+++ src/GameManager.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class GameManager {
+    private int days;
+    private ArrayList<Location> listOfLocations;
+    private boolean gameStatus;
+    private TextPrinter tp;
+    public GameManager() {
+
+
+    }
+
+    public boolean getGameStatus() {
+        return this.gameStatus;
+    }
+
+
+}
---
+++ src/TextPrinter.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TextPrinter {
+
+    public void printDialog(String Dialog) {
+        System.out.print(Dialog + "\n" + "> ");
+        Scanner in = new Scanner(System.in);
+
+    }
+
+}
