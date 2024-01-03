e318ca7f55486e8c4a8424318c633fa7da291445
Abudlrahman Almajdalawi <at.majdalawi@gmail.com>
2024-01-03 05:37:04 +0100

    Add more choices to THLocation

--- src/THLocation.java
+++ src/THLocation.java
@@ -4,6 +4,7 @@ public class THLocation extends Location{
     private ArrayList<Choice> currentChoices;
     String locationName;
     private String currentDialogue;
+    private int daysSince;
 
 
     public THLocation() {
@@ -12,6 +13,9 @@ public class THLocation extends Location{
         this.currentDialogue = """
                 You are now at the Town Hall, what do you want to do?""";
         Choice c1 = new Choice("get ticket", "wait");
+        Choice c2 = new Choice("return home", "goToBase");
+        Choice c3 = new Choice("argue", "addToInventory", "Annex 15");
+        Choice c4 = new Choice("send email", "wait");
         this.currentChoices.add(c1);
 
     }
