d7bf479ffe52106d3d6922d4784d256e9b45b5d5
Abudlrahman Almajdalawi <at.majdalawi@gmail.com>
2024-01-03 16:03:13 +0100

    quick modification to the current dialogue of FirstLocation

--- src/FirstLocation.java
+++ src/FirstLocation.java
@@ -13,6 +13,9 @@ public class FirstLocation extends Location {
                 Welcome to Brussels. As an international student, you are required to get your residence 
                 permit in 8 days, and thus must get some items necessary to get your permit, if you fail to
                 do so, you will lose and get kicked out of the country!
+                To get choices available, type help. 
+                To display items in inventory, type inventory.
+                To quit the game, type quit.
                 Tread wisely!""" ;
         Choice choice1 = new Choice("proceed", "goToBase");
         currentChoices.add(choice1);
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
         GameManager gm = new GameManager();
         while (gm.getGameStatus())
         {
